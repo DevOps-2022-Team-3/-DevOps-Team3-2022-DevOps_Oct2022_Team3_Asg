7bea6e21e9e49dfe157f044ef3fa279cff96fc50
aykenlee <73160980+aykenlee@users.noreply.github.com>
2023-02-02 18:55:47 +0800

    Route templates

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 
-appFlask = Flask(__name__)
-@appFlask.route('/Main')
+flaskApp = Flask(__name__)
 
-def main_page():
-    return render_template('Main.html')
+# @flaskApp.route('/')
+
+@flaskApp.route('/Main')
+def main():
+    return render_template('main.html')
+
+@flaskApp.route('/Upload_Data')
+def upload_data():
+    return render_template('upload_data.html')
+
+@flaskApp.route('/Match_Student')
+def match_student():
+    return render_template('match_student.html')
+
+@flaskApp.route('/Prepare_Email')
+def prepare_email():
+    return render_template('prepare_email.html')
+
+@flaskApp.route('/Settings')
+def settings():
+    return render_template('settings.html')
+
 
 if __name__ == "__main__":
-    appFlask.run(host='0.0.0.0', port=5221)
+    flaskApp.run(debug=True, host='0.0.0.0', port=5221)
