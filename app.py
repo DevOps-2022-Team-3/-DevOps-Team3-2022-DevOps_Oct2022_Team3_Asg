220e634da5973ed54b58d17b1a9fcfd88b879cb8
Ho-Jun-An <Jun-An@Live.com>
2023-02-04 17:02:21 +0800

    Added database and partially implemented relevant code

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
 from flask import Flask, request, render_template, url_for
-import mysql.connector
-from mysql.connector import Error
-import pandas as pd
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 flaskApp = Flask(__name__)
+
+##### Database Code #####
+flaskApp.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+flaskApp.config['SECRET_KEY'] = "secret key"
+db = SQLAlchemy(flaskApp)
+
+class Student(db.Model):
+    id = db.Column(db.String(10), primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    preference = db.Column(db.String(200), nullable=False)
+    status = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<ID %r>' % self.id
+
+class StudentForm(FlaskForm):
+    id = StringField(validations=[DataRequired()])
+    name = StringField(validations=[DataRequired()])
+    preference = StringField(validations=[DataRequired()])
+    status = StringField(validations=[DataRequired()])
+
+with flaskApp.app_context():
+    db.create_all()
+##### Database Code #####
 
 @flaskApp.route('/')
 
@@ -15,8 +43,9 @@ def main():
 def upload_data():
     return render_template('upload_data.html')
 
-@flaskApp.route('/Match_Student')
+@flaskApp.route('/Match_Student', methods=['GET', 'POST'])
 def match_student():
+    form = StudentForm()
     return render_template('match_student.html')
 
 @flaskApp.route('/Prepare_Email')
@@ -27,23 +56,5 @@ def prepare_email():
 def settings():
     return render_template('settings.html')
 
-
 if __name__ == "__main__":
     flaskApp.run(debug=True, host='0.0.0.0', port=5221)
-
-
-
-# Code for Database
-def create_server_connection(host_name, user_name, user_password):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password
-        )
-        print("MySQL Database connection successful")
-    except Error as err:
-        print(f"Error: '{err}'")
-
-    return connection
