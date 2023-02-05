64cb603cef1351d3e7cf8c708018c0a254454fa2
aykenlee <73160980+aykenlee@users.noreply.github.com>
2023-02-05 17:27:50 +0800

    Read excel and insert to database

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, render_template, url_for
+import os
+from os.path import join, dirname, realpath
+from flask import Flask, request, render_template, url_for, flash, redirect
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
@@ -7,8 +9,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
-from werkzeug.utils import secure_filename
-from werkzeug.datastructures import  FileStorage
+import pandas as pd
 
 flaskApp = Flask(__name__)
 
@@ -58,16 +59,57 @@ with flaskApp.app_context():
 def main():
     return render_template('main.html')
 
+##### Data Upload Code #####
 @flaskApp.route('/Upload_Data')
 def upload_data():
     return render_template('upload_data.html')
 
-@flaskApp.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return render_template('upload_data.html')
+# Upload folder
+UPLOAD_FOLDER = 'static/files'
+flaskApp.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
+
+# File upload for student data
+@flaskApp.route('/student_uploader', methods = ['GET', 'POST'])
+def upload_student_file():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            student_col = ['id', 'name', 'preference']
+            studentData = pd.read_excel(uploaded_file, names=student_col, header=None)
+            for i, row in studentData.iterrows():
+                id = str(row[0])
+                name = str(row[1])
+                preference = str(row[2])
+                status = 'unassigned'
+                student = Student(id=id,
+                                name=name,
+                                preference=preference,
+                                status=status)
+                db.session.add(student)
+                db.session.commit()
+        return redirect('Upload_Data')
+
+# File upload for company data
+@flaskApp.route('/company_uploader', methods = ['GET', 'POST'])
+def upload_company_file():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            company_col = ['name', 'role', 'contact', 'email']
+            companyData = pd.read_excel(uploaded_file, names=company_col, header=None)
+            for i, row in companyData.iterrows():
+                name = str(row[0])
+                role = str(row[1])
+                contact = str(row[2])
+                email = 'unassigned'
+                company = Student(name=name,
+                                role=role,
+                                contact=contact,
+                                email=email)
+                db.session.add(company)
+                db.session.commit()
+        return redirect('Upload_Data')
+########################################
 
 @flaskApp.route('/Match_Student', methods=['GET', 'POST'])
 def match_student():
