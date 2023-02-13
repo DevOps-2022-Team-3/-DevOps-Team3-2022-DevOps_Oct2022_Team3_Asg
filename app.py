50747c271841377dcffd51c5ca8680108b0de5c0
Sharuk16 <S10194023@connect.np.edu.sg>
2023-02-14 06:47:07 +0800

    Data Validation and Automation

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ from sqlalchemy.sql import text
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
-
+import requests
+import os
 
 
 flaskApp = Flask(__name__)
+
+webhook_url = "https://www.workato.com/webhooks/rest/a30675cf-588c-41c6-992c-d83810677fd7/changesets"
 
 ##### Database Code #####
 flaskApp.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
@@ -19,8 +22,8 @@ db = SQLAlchemy(flaskApp)
 
 class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    resume_dir = db.Column(db.LargeBinary)  #store raw data
-    email_dir = db.Column(db.LargeBinary)
+    resume_dir = db.Column(db.String(200))  #store raw data
+    email_dir = db.Column(db.String(200))
     internship_period = db.Column(db.String(120))
 
     def __init__(self, resume_dir, email_dir, internship_period, methods=['GET','POST']):
@@ -66,6 +69,8 @@ with flaskApp.app_context():
 
 @flaskApp.route('/')
 
+
+
 @flaskApp.route('/Main')
 def main():
     return render_template('main.html')
@@ -95,15 +100,40 @@ def match_student():
 def prepare_email():
     return render_template('prepare_email.html')
 
+
+
+#data validation for resume and email
+ALLOWED_EXTENSIONS = set(['csv', 'pdf'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
+
 @flaskApp.route('/Settings', methods=['GET','POST'])
 def settings():
     if request.method == 'POST':
-        resume_dir = request.files['resume_dir'].read() 
-        email_dir = request.files['email_dir'].read()
+        resume_dir = request.files.get('resume_dir') 
+        email_dir = request.files.get('email_dir')
         internship_period = request.form['Internship_Period']
 
-        # Store the values in the database
-        file = File(resume_dir, email_dir, internship_period)
+        #automation
+        payload = {"internship_period": internship_period}
+
+        response = requests.post(webhook_url, json=payload)
+
+        print(response.status_code)
+
+        #checks the file extension before reading the file
+
+        if not resume_dir or not allowed_file(resume_dir.filename):
+            return render_template("settings.html", error_message="Invalid resume file type. Only CSV and PDF files are allowed.")
+        if not email_dir or not allowed_file(email_dir.filename):
+            return render_template("settings.html", error_message="Invalid email file type. Only CSV and PDF files are allowed.")
+        
+        # Store the path of the file in the database instead of the actual file
+        file = File(resume_dir=resume_dir.filename, email_dir=email_dir.filename, internship_period=internship_period)
         db.session.add(file)
         db.session.commit()
     return render_template('settings.html')
